7b1f7c728314e3f8ba011dcde3e744a9aa08dc3c
tonyaellie <tonya@tokia.dev>
2023-06-13 17:37:29 +0000

    feat: updates and stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="hero min-h-screen bg-base-200">
+    <div className="hero h-auto bg-base-200">
       <div className="hero-content text-center">
         <div className="max-w-md">
           <h1 className="text-5xl font-bold">
